ffd12b15029dd0b934fbd4eeda9a2bb6af1136f9
Filip Vukovinski <filip.vukovinski@net.hr>
2025-07-01 19:04:40 +0200

    hide useless search bar

--- Shopnetic.Admin/ClientApp/src/components/Layout/Header.tsx
+++ Shopnetic.Admin/ClientApp/src/components/Layout/Header.tsx
@@ -17,14 +17,14 @@ const Header: React.FC<HeaderProps> = ({ onMenuToggle }) => {
             <Menu size={20} />
           </button>
           
-          <div className="relative hidden sm:block">
+          {/* <div className="relative hidden sm:block">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
             <input
               type="text"
               placeholder="Search..."
               className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
-          </div>
+          </div> */}
         </div>
         
         <div className="flex items-center gap-4">
