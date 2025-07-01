07be9288b46e4276508ac2edae67e931ab65b7a6
Filip Vukovinski <filip.vukovinski@net.hr>
2025-07-01 09:29:32 +0200

    Some work on admin app

--- Shopnetic.Admin/ClientApp/src/server.ts
+++ Shopnetic.Admin/ClientApp/src/server.ts
@@ -8,8 +8,26 @@ export const server = {
             .then(data => data as Product)
             .catch(error => console.log(error))
         },
-        editProduct: async (editProductRequest: EditProductRequest) => {
-
+        editProduct : async (editProductRequest: Product) : Promise<Boolean | void> => {
+            return fetch(`https://${hostname}/products/edit`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(editProductRequest)
+            })
+            .then(resp => resp.json())
+            .then(data => data as Boolean)
+            .catch(error => console.log(error))
+        },
+        saveProductImage : async (productId: number, imageFile: File) : Promise<number | void> => {
+            const formData = new FormData();
+            formData.append('imageFile', imageFile);
+            return fetch(`https://${hostname}/products/${productId}/uploadimage`, {
+                method: 'POST',
+                body: formData
+            })
+            .then(resp => resp.json())
+            .then(data => data as number)
+            .catch(error => console.log(error))
         }
     }
 }
@@ -19,20 +37,22 @@ interface Product
     id: number,
     name: string,
     description: string
+    category: {
+        categoryId: number,
+        categoryName: string
+    },
     currentPrice: {
-        productPriceId: number,
-        price: number,
-        effectiveFrom: string,
-        effectiveTo: string
+        price: number
     },
     inventory: {
-        productInventoryId: number,
         quantity: number,
-        lastUpdated: string
+        lowStockThreshold: number
     }
-}
-
-interface EditProductRequest
-{
-
+    status: 'Active' | 'Inactive',
+    sku: string,
+    images: [{
+        imageId: number,
+        primary: Boolean,
+        imageUrl: string
+    }]
 }
