c7acadcb3c1c480ca47c410febc9312e656fb0fb
Filip Vukovinski <filip.vukovinski@net.hr>
2025-07-02 18:34:26 +0200

    embedded files middleware

---
+++ Shopnetic.Admin/EmbeddedFilesMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Shopnetic.Admin;
+
+public class EmbeddedFilesMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+    private readonly FileExtensionContentTypeProvider _contentTypes = new();
+
+    public EmbeddedFilesMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var path = context.Request.Path.Value?.TrimStart('/') ?? "index.html";
+        if (string.IsNullOrWhiteSpace(path)) path = "index.html";
+
+        var resourceName = $"Shopnetic.Admin.ClientApp.dist.{path.Replace("/", ".")}";
+
+        using var stream = _assembly.GetManifestResourceStream(resourceName);
+        if (stream == null)
+        {
+            await _next(context);
+        }
+        else
+        {
+            var fileName = Path.GetFileName(path);
+            if (!_contentTypes.TryGetContentType(fileName, out var contentType))
+                contentType = "application/octet-stream";
+
+            context.Response.ContentType = contentType;
+            await stream!.CopyToAsync(context.Response.Body);
+        }
+    }
+}
--- Shopnetic.Admin/Program.cs
+++ Shopnetic.Admin/Program.cs
@@ -1,24 +1,14 @@
-using System.Reflection;
+using Shopnetic.Admin;
 using Shopnetic.Shared.Database;
-using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddShopneticDbContext(builder.Configuration);
 builder.Services.AddControllers();
 var app = builder.Build();
 
-var embeddedFileProvider = new ManifestEmbeddedFileProvider(
-    Assembly.GetExecutingAssembly(),
-    "ClientApp"
-);
-
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = embeddedFileProvider,
-    RequestPath = ""
-});
-
 app.UseHttpsRedirection();
+app.UseMiddleware<EmbeddedFilesMiddleware>();
+app.MapFallbackToFile("index.html");
 app.MapControllers();
 app.Run();
 
