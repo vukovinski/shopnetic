fe783e8dea91de449ced260e6d9b73093fa60dcb
Filip Vukovinski <filip.vukovinski@net.hr>
2025-07-03 13:44:53 +0200

    Some work on admin app

---
+++ Shopnetic.Admin/Controllers/OrdersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Shopnetic.Admin.Dto;
+using Shopnetic.Shared.Database;
+
+namespace Shopnetic.Admin.Controllers
+{
+    [ApiController]
+    [Route("api/orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ShopneticDbContext _context;
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(ShopneticDbContext context, ILogger<OrdersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrders()
+        {
+            var orders = await _context.Orders.ToListAsync();
+            var mappedOrders = orders.Select(o => new OrderDto
+            {
+                orderId = o.OrderId,
+                orderDate = o.CreatedAt,
+                status = "Created",
+                customerName = o.ShippingAddress.FullName,
+                totalAmount = o.OrderTotal,
+                items = o.OrderItems.Select(oi => new OrderItemDto
+                {
+                    orderItemId = oi.OrderItemId,
+                    productId = oi.ProductId,
+                    productName = oi.Product.ProductName,
+                    price = oi.Price,
+                    quantity = oi.Quantity
+
+                }).ToList()
+
+            }).ToList();
+            return Ok(mappedOrders);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditOrder([FromBody] OrderDto order)
+        {
+            // TODO: edit order
+            return Ok();
+        }
+    }
+}
